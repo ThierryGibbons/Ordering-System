cb566bb4f6d20a736f5eb412694756d759e0fc54
Thierry <270067227@yoobeestudent.ac.nz>
2023-03-01 11:23:56 +1300

    accepted incoming changes

--- Ordering-System/main.cpp
+++ Ordering-System/main.cpp
@@ -31,39 +31,21 @@ int main() {
 
     vector<menuItem> items;
 
-<<<<<<< Updated upstream
     for (int i = 1; i < 6; i++) {
-=======
-    for (int i = 1; i < 4; i++) {
->>>>>>> Stashed changes
         menuItem item = menu.getMenuItem(i);
         items.push_back(item);
     }
-    
+
     menu.getMenu();
 
     //orders.createOrder(1,items);
-    
+
     orders.getOrder(4);
 
-<<<<<<< Updated upstream
     orders.confirmOrder(10);
     orders.getOrder(10);
 
     orders.cancelOrder(50);
-=======
-    menu.modifiyMenu(2);
-
-    menu.getMenuItem(2);
-    //orders.confirmOrder(49);
-    
-
-    orders.confirmOrder(6);
-    //orders.getOrder(10);
-    
-
-    //orders.cancelOrder(50);
->>>>>>> Stashed changes
 
     system("pause>0");//system will be removed just here for testing
     //return 0;
